c95898573e12f24c422e19e2f97eec779f90d3f0
mattg <mattgal2002@gmail.com>
2020-06-04 21:07:11 -0600

    finally fixed navbar and add same font to body

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ const express = require('express')
 const morgan = require('morgan')
 const mailgun = require('mailgun-js')
 const bodyParser = require('body-parser')
+const {check, validationResult} = require("express-validator")
+const Recaptcha = require('express-recaptcha').RecaptchaV2
+
 
 //initializing the express application//
-
 const app = express()
 
 //project wide middleware declerations for express//
@@ -13,8 +15,14 @@ app.use(express.json())
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
-
+const recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY)
 const indexRoute = express.Router()
+
+const requestValidation = [
+	check("email", "A Valid Email is required").isEmail().normalizeEmail(),
+	check('name', "A name is required to send an email").not().isEmpty().trim().escape(),
+	check('subject').optional().trim().escape(),
+	check('message', 'A message is required to send email').not().isEmpty().trim().escape().isLength({max: 2000}),
 
 indexRoute.route('/apis').get((request, response)=>{
 return response.json('hello')
@@ -24,8 +32,8 @@ return response.json('hello')
 	response.append('Access-Control-Allow-Origin', 'value:['*']');
 	console.log(request.body)
 	return response.json('is this thing on?')
-})
-app.use(indexRoute)
-app.listen(4200, () => {console.log("the server has started")})
+}),
+app.use(indexRoute),
+app.listen(4200, () => {console.log("the server has started")})]
 
 
